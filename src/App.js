002fd9ec4b8ae3c1f83168095fac01a3ad4d2dbc
TheJeme <eemelijoonatan@gmail.com>
2024-11-11 19:40:04 +0200

    mega epic

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { ReactSketchCanvas } from 'react-sketch-canvas';
+import { useRef, useEffect, useState } from 'react';
 function App() {
+  const [color, setColor] = useState('#000000');
+  const canvas = useRef(null);
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      const key = event.key.toLowerCase();
+      if (event.ctrlKey && key === 'z') {
+        canvas.current?.undo()
+      } else if (event.ctrlKey && key === 'y') {
+        canvas.current?.redo()
+      } else if (key === '1') {
+        setColor('#000000')
+      } else if (key === '2') {
+        setColor('#ff0000')
+      } else if (key === '3') {
+        setColor('#00ff00')
+      } else if (key === '4') {
+        setColor('#0000ff')
+      } else if (key === '5') {
+        setColor('#ff00ff')
+      }
+    };
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ReactSketchCanvas
+      ref={canvas}
+      strokeWidth={4}
+      canvasColor="#eee"
+      strokeColor={color}
+    />
   );
 }
 
